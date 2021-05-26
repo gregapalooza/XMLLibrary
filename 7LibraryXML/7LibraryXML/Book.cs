2de3de1cc2c7402f456e5e55be62526c4b9a86fa
Greg Vaggalis <58676443+gregapalooza@users.noreply.github.com>
2021-05-25 19:22:52 -0500

    Add files via upload

---
+++ 7LibraryXML/7LibraryXML/Book.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace _7LibraryXML
+{
+    class Book
+    {
+        #region ---- Private class fields ----
+        private string bookTitle;
+        private string bookAuthor;
+        private string bookSubject;
+        private string bookISBN;
+        private string dateAdded;
+        private int numCopies;
+        private int copiesCheckedOut;
+        #endregion
+        #region ---- Getter and setters ----
+        public string BookTitle { get => bookTitle; set { bookTitle = value; } }
+        public string BookAuthor { get => bookAuthor; set {bookAuthor = value; } }
+        public string BookSubject { get => bookSubject; set {bookSubject = value; } }
+        public string BookISBN { get => bookISBN; set { bookISBN = value; } }
+        public string DateAdded { get => dateAdded; set { dateAdded = value; } }
+        public int NumCopies { get => numCopies; set { numCopies = value; } }
+        public int CopiesCheckedOut { get => copiesCheckedOut; set { copiesCheckedOut = value; } }
+        #endregion
+        #region ---- Constructors ----
+        public Book() { }
+        public Book(string bookTitle, string bookAuthor, string bookSubject, 
+            string bookISBN, string dateAdded, int numCopies, int copiesCheckedOut)
+        {
+            BookTitle = bookTitle;
+            BookAuthor = bookAuthor;
+            BookSubject = bookSubject;
+            BookISBN = bookISBN;
+            DateAdded = dateAdded;
+            NumCopies = numCopies;
+            CopiesCheckedOut = copiesCheckedOut;
+        }
+        #endregion
+        #region ---- Class Methods ----
+        public override string ToString()
+        {
+            string bookInfo = "";
+            bookInfo = $"Book: {BookTitle}  Author: {BookAuthor} | Subject: {BookSubject}  " +
+                $"ISBN: {BookISBN} Date Added: {DateAdded} | Num Copies {NumCopies}";
+            return bookInfo;
+        }
+        /// <summary>
+        /// This method compares the amount of books overall to the
+        /// ones check out to determine if there are any 
+        /// left to check out.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsBookAvailable()
+        {
+            return (copiesCheckedOut < numCopies);
+        }
+        /// <summary>
+        /// Based off the answer of book availibility 
+        /// this method checks out a book and gives a 
+        /// return date for the book
+        /// </summary>
+        public void CheckOutBook()
+        {
+            DateTime today = DateTime.Now;
+            if (IsBookAvailable())
+            {
+                CopiesCheckedOut++;
+                MessageBox.Show($"Book is available. It is due back in 2 weeks: {today.AddDays(14)}.");
+                //.AddDays(14) gives the user 2 weeks to read the book
+            }
+            else
+            {
+                MessageBox.Show("We're sorry, all copies are check out at the moment.");
+                
+            }
+        }
+        /// <summary>
+        /// This method allows for the user to check a book back in
+        /// doesn't allow for 'over returns'
+        /// </summary>
+        public void CheckInBook()
+        {
+            if (copiesCheckedOut < numCopies && copiesCheckedOut != 0)
+            {
+                copiesCheckedOut--;
+                MessageBox.Show("The book has been returned. Thank you!");
+            }
+            else
+            {
+                MessageBox.Show("All books are accounted for.");
+            }
+        }
+        #endregion
+    }
+}
---
+++ 7LibraryXML/7LibraryXML/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace _7LibraryXML
+{
+    public partial class Form1 : Form
+    {
+        //string variable for xml file
+        string xmlName = "Books.xml";
+        #region ----- Form control methods -----
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PutBooksOnShelf();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Checkout();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReturnBook();
+        }
+        #endregion
+        #region ----- Other methods -----
+        /// <summary>
+        /// This method loads the xml file to 
+        /// listBox control to view all the books
+        /// </summary>
+        private void PutBooksOnShelf()
+        {
+            var Books = new List<Book>();;
+            // try/catch to help program not crash instantly
+            try
+            {
+                var xml = XDocument.Load(xmlName).Root;
+
+                foreach (var bk in xml.Elements()) //cycle through each book in xml file
+                {
+                    var Book = new Book();
+
+                    Book.BookTitle = bk.Elements().Where(x => x.Name == "title").FirstOrDefault().Value.ToString();
+                    Book.BookAuthor = bk.Elements().Where(x => x.Name == "author").FirstOrDefault().Value.ToString();
+                    Book.BookSubject = bk.Elements().Where(x => x.Name == "subject").FirstOrDefault().Value.ToString();
+                    Book.BookISBN = bk.Elements().Where(x => x.Name == "isbn").FirstOrDefault().Value.ToString();
+                    Book.DateAdded = bk.Elements().Where(x => x.Name == "dateadded").FirstOrDefault().Value.ToString();
+                    Book.NumCopies = Convert.ToInt16(bk.Elements().Where(x => x.Name == "numcopies").FirstOrDefault().Value);
+
+                    listBox1.Items.Add(Book);
+                }
+            }
+            catch (Exception e)
+            {
+                listBox1.Items.Add(e.Message);
+            }
+
+        }
+        /// <summary>
+        /// This method control the check out button on the form
+        /// </summary>
+        public void Checkout()
+        {
+            try
+            {
+                Book thisBook = (Book)listBox1.Items[listBox1.SelectedIndex]; //user needs to click on a selection first this helps prevent this error
+                thisBook.CheckOutBook();
+                var xml = XDocument.Load(xmlName).Root;
+                foreach (var bk in xml.Elements())
+                {
+                    if (bk.Elements().Where(x => x.Name == "title").FirstOrDefault().Value.ToString() == thisBook.BookTitle)
+                    {
+                        bk.Elements().Where(x => x.Name == "numcopies").FirstOrDefault().Value = thisBook.NumCopies.ToString();
+                    }
+                }
+
+                xml.Save(xmlName);
+            }
+            catch (Exception e)
+            { 
+                MessageBox.Show(e.Message); //Better to show as a messagebox than add to listBox
+            }
+        }
+        /// <summary>
+        /// This method controls the return button on form
+        /// works the same as check out method
+        /// </summary>
+        public void ReturnBook()
+        {
+            try
+            {
+                Book thisBook = (Book)listBox1.Items[listBox1.SelectedIndex];
+                thisBook.CheckInBook();
+                var xml = XDocument.Load(xmlName).Root;
+                foreach (var bk in xml.Elements())
+                {
+                    if (bk.Elements().Where(x => x.Name == "title").FirstOrDefault().Value.ToString() == thisBook.BookTitle)
+                    {
+                        bk.Elements().Where(x => x.Name == "numcopies").FirstOrDefault().Value = thisBook.NumCopies.ToString();
+                    }
+                }
+
+                xml.Save(xmlName);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        #endregion
+    }
+}
